cd4faefdd6a4235aed25d19d86bca21b99a93ace
snarkusius <marcus@lengquist.se>
2020-09-28 14:25:43 +0200

    inputFeald

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const button = document.getElementById("button")
+const button = document.getElementById("inputfield")
 button.onclick = function(){alert("hej")} 
 button.onmouseenter =function(){alert("nope")} 
